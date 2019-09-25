729975a8e8f039c9452b9e71338130475a48e0fb
q39676 <396767101@qq.com>
2019-09-25 12:37:07 +0800

    .

---
+++ src/main/java/movie/pojo/constant/MovieInteractionUrl.java
@@ -0,0 +1,8 @@
+package movie.pojo.constant;
+
+public class MovieInteractionUrl {
+	
+	public static final String root = "/movieInteraction";
+	public static final String simpleList = "/simpleList";
+
+}
