ce6af99e093611230c6bd121200f98a3d05bac66
q39676 <396767101@qq.com>
2019-10-30 16:35:55 +0800

    .

--- src/main/java/movie/pojo/type/MovieRegionType.java
+++ src/main/java/movie/pojo/type/MovieRegionType.java
@@ -3,25 +3,27 @@ package movie.pojo.type;
 public enum MovieRegionType {
 	
 	/** 国内(大陆)电影 */
-	domestic("domestic", 3),
+	domestic("domestic", 3, "国内(大陆)电影"),
 	/** 欧美电影 */
-	eurAndAmerica("eurAndUmerica", 4),
+	eurAndAmerica("eurAndUmerica", 4, "欧美电影"),
 	/** 日韩电影 */
-	jpAndKr("jpAndKr", 5),
+	jpAndKr("jpAndKr", 5, "日韩电影"),
 	/** 港澳台电影 */
-	hongKongMarcoTaiwan("hongKongMarcoTaiwan", 7),
+	hongKongMarcoTaiwan("hongKongMarcoTaiwan", 7, "港澳台电影"),
 	
 	/** 其他电影 */
-	otherMovie("otherMovie", 6),
+	otherMovie("otherMovie", 6, "其他电影"),
 	
 	;
 	
 	private String name;
 	private Integer code;
+	private String remark;
 	
-	MovieRegionType(String evaluationName, Integer evaluationCode) {
+	MovieRegionType(String evaluationName, Integer evaluationCode, String remark) {
 		this.name = evaluationName;
 		this.code = evaluationCode;
+		this.remark = remark;
 	}
 	
 
@@ -31,6 +33,10 @@ public enum MovieRegionType {
 
 	public Integer getCode() {
 		return code;
+	}
+	
+	public String getRemark() {
+		return remark;
 	}
 
 	public static MovieRegionType getType(String typeName) {
