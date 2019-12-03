2e3790718c4ab929985358861e94d8b7b5e111f4
q39676 <396767101@qq.com>
2019-12-03 17:13:55 +0800

    .

---
+++ src/main/java/auxiliaryCommon/autoTest/jsonReport/pojo/constant/ServerHost.java
@@ -0,0 +1,8 @@
+package auxiliaryCommon.autoTest.jsonReport.pojo.constant;
+
+public class ServerHost {
+
+	public static final String host1 = "http://127.0.0.1:10001";
+	public static final String host2 = "http://127.0.0.1:10002";
+	
+}
