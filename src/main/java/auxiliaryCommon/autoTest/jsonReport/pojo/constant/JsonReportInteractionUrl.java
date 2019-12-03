54fe3f287c44428cbab4ade7fa408b549aa2e2ad
q39676 <396767101@qq.com>
2019-12-03 16:36:28 +0800

    .

---
+++ src/main/java/auxiliaryCommon/autoTest/jsonReport/pojo/constant/JsonReportInteractionUrl.java
@@ -0,0 +1,9 @@
+package auxiliaryCommon.autoTest.jsonReport.pojo.constant;
+
+public class JsonReportInteractionUrl {
+
+	public static final String root = "/jsonReportInteraction";
+	
+	public static final String findReportsByCondition = "/findReportsByCondition";
+	public static final String findReportByTestEventId = "/findReportByTestEventId";
+}
