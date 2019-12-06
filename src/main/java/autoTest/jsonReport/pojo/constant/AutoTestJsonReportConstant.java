f23740092d22c7a395d0d3ebc7f467357b4fc0d9
q39676 <396767101@qq.com>
2019-12-06 14:06:43 +0800

    .

---
+++ src/main/java/autoTest/jsonReport/pojo/constant/AutoTestJsonReportConstant.java
@@ -0,0 +1,8 @@
+package autoTest.jsonReport.pojo.constant;
+
+public class AutoTestJsonReportConstant {
+
+	public static final String strKey = "strKey";
+	public static final String imgKey = "imgKey";
+	
+}
