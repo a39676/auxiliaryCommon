3b00f42832b1d471e572b73e974c467202d34039
DavenChan <396767101@qq.com>
2019-12-09 08:16:13 +0800

    .

--- src/main/java/autoTest/jsonReport/pojo/constant/AutoTestInteractionUrl.java
+++ src/main/java/autoTest/jsonReport/pojo/constant/AutoTestInteractionUrl.java
@@ -1,9 +1,11 @@
 package autoTest.jsonReport.pojo.constant;
 
-public class JsonReportInteractionUrl {
+public class AutoTestInteractionUrl {
 
 	public static final String root = "/jsonReportInteraction";
 	
 	public static final String findReportsByCondition = "/findReportsByCondition";
 	public static final String findReportByTestEventId = "/findReportByTestEventId";
+	
+	public static final String insertBingDemoTestEvent = "/insertBingDemoTestEvent";
 }
