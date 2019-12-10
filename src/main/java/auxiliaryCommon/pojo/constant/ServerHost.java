9e38ea70b7ec2901684b6df3dd6085f48392545a
q39676 <396767101@qq.com>
2019-12-10 17:49:25 +0800

    .

--- src/main/java/auxiliaryCommon/pojo/constant/ServerHost.java
+++ src/main/java/auxiliaryCommon/pojo/constant/ServerHost.java
@@ -2,7 +2,7 @@ package auxiliaryCommon.pojo.constant;
 
 public class ServerHost {
 
-	public static final String host1 = "http://127.0.0.1:10001";
-	public static final String host2 = "http://127.0.0.1:10002";
+	public static final String localHost10001 = "http://127.0.0.1:10001";
+	public static final String localHost10002 = "http://127.0.0.1:10002";
 	
 }
