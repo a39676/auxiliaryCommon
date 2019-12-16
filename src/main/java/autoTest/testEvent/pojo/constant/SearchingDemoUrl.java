2af28b79d2f4f69ebc9e9c15e98a512cb05f8ea3
Abc <Abc@Abc-PC>
2019-12-16 17:40:40 +0800

    .

--- src/main/java/autoTest/testEvent/pojo/constant/SearchingDemoUrl.java
+++ src/main/java/autoTest/testEvent/pojo/constant/SearchingDemoUrl.java
@@ -5,5 +5,4 @@ public class SearchingDemoUrl {
 	public static final String root = "/searchingDemo";
 	
 	public static final String insert = "/insert";
-	public static final String run = "/run";
 }
