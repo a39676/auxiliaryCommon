f391dc901125d8131d0a0bc9738463e5693b097b
DavenChan <396767101@qq.com>
2020-01-31 13:34:29 +0800

    .

---
+++ src/main/java/autoTest/pojo/constant/AutoTestUrl.java
@@ -0,0 +1,9 @@
+package autoTest.pojo.constant;
+
+public class AutoTestUrl {
+
+	public static final String root = "/atDemo";
+	
+	public static final String linkToATHome = "/linkToATHome";
+	public static final String index = "/index";
+}
