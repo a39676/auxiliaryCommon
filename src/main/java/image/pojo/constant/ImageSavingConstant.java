574499a2d4a71741b4882ba4ac9dd5802fbe5150
DavenChan <396767101@qq.com>
2020-02-28 11:42:08 +0800

    .

---
+++ src/main/java/image/pojo/constant/ImageSavingConstant.java
@@ -0,0 +1,8 @@
+package image.pojo.constant;
+
+public class ImageSavingConstant {
+
+	public static final String imageSavingFolderName = "/imageSaving";
+	public static final String imgSavingPath = "/home/u2" + imageSavingFolderName;
+	
+}
