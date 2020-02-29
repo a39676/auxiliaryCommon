abf11dae3a392fc9d72285727d6d2a93b6eba7f5
DavenChan <396767101@qq.com>
2020-02-29 16:17:44 +0800

    .

---
+++ src/main/java/selenium/pojo/constant/SeleniumConstant.java
@@ -0,0 +1,10 @@
+package selenium.pojo.constant;
+
+import movie.pojo.constant.MovieInteractionConstant;
+
+public class SeleniumConstant {
+	
+	/** 最大历史留存时间 */
+	public static final int maxHistoryMonth = MovieInteractionConstant.maxQueryHistoryMonth + 2;
+	
+}
