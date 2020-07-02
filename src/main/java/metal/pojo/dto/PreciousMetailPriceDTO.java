c9a7baf957d17e5cfb627e11afccd3a7e9d6d581
DavenChan <396767101@qq.com>
2020-07-02 15:24:34 +0800

    .

---
+++ src/main/java/metal/pojo/dto/PreciousMetailPriceDTO.java
@@ -0,0 +1,49 @@
+package metal.pojo.dto;
+
+import metal.pojo.type.MetalType;
+import tool.pojo.type.UtilOfWeightType;
+
+public class PreciousMetailPriceDTO {
+
+	/**
+	 * {@link MetalType}
+	 */
+	private Integer metalType;
+	
+	/**
+	 * {@link UtilOfWeightType}
+	 */
+	private Integer weightUtilType;
+	private Double price;
+
+	public Integer getMetalType() {
+		return metalType;
+	}
+
+	public void setMetalType(Integer metalType) {
+		this.metalType = metalType;
+	}
+
+	public Integer getWeightUtilType() {
+		return weightUtilType;
+	}
+
+	public void setWeightUtilType(Integer weightUtilType) {
+		this.weightUtilType = weightUtilType;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "PreciousMetailPriceDTO [metalType=" + metalType + ", weightUtilType=" + weightUtilType + ", price="
+				+ price + "]";
+	}
+
+}
