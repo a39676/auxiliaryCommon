7f8912b68c18970987202c10c394355238a45df6
DavenChan <396767101@qq.com>
2020-07-10 10:50:16 +0800

    .

---
+++ src/main/java/precious_metal/pojo/constant/PreciousMetalConstant.java
@@ -0,0 +1,9 @@
+package precious_metal.pojo.constant;
+
+import java.math.BigDecimal;
+
+public class PreciousMetalConstant {
+
+	public static final BigDecimal goleOunceToGram = new BigDecimal(31.1034768);
+	
+}
