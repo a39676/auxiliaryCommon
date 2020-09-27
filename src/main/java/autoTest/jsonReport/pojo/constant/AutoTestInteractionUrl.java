3403b778baf0f42445304f4fb56a167408a19f2f
DavenChan <396767101@qq.com>
2020-09-28 00:51:20 +0800

    .

--- src/main/java/autoTest/jsonReport/pojo/constant/AutoTestInteractionUrl.java
+++ src/main/java/autoTest/jsonReport/pojo/constant/AutoTestInteractionUrl.java
@@ -2,10 +2,10 @@ package autoTest.jsonReport.pojo.constant;
 
 public class AutoTestInteractionUrl {
 
-	public static final String root = "/jsonReportInteraction";
+	public static final String ROOT = "/jsonReportInteraction";
 	
-	public static final String findReportsByCondition = "/findReportsByCondition";
-	public static final String findReportByTestEventId = "/findReportByTestEventId";
+	public static final String FIND_REPORTS_BY_CONDITION = "/findReportsByCondition";
+	public static final String FIND_REPORT_BY_TEST_EVENT_ID = "/findReportByTestEventId";
 	
-	public static final String insertSearchingDemoTestEvent = "/insertSearchingDemoTestEvent";
+	public static final String INSERT_SEARCHING_DEMO_TEST_EVENT = "/insertSearchingDemoTestEvent";
 }
