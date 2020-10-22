bbf8ec64d8e2758fd6b6e377cbd98c76f51b745f
DavenChan <396767101@qq.com>
2020-10-22 15:28:21 +0800

    .

--- src/main/java/finance/cryptoCoin/pojo/dto/CryptoCoinHistoryPriceDTO.java
+++ src/main/java/finance/cryptoCoin/pojo/dto/CryptoCoinHistoryPriceDTO.java
@@ -4,7 +4,27 @@ import java.util.List;
 
 public class CryptoCoinHistoryPriceDTO {
 
+	private String cryptoCoinTypeName;
+
+	private String currencyName;
+
 	private List<CryptoCoinHistoryPriceSubDTO> priceHistoryData;
+
+	public String getCryptoCoinTypeName() {
+		return cryptoCoinTypeName;
+	}
+
+	public void setCryptoCoinTypeName(String cryptoCoinTypeName) {
+		this.cryptoCoinTypeName = cryptoCoinTypeName;
+	}
+
+	public String getCurrencyName() {
+		return currencyName;
+	}
+
+	public void setCurrencyName(String currencyName) {
+		this.currencyName = currencyName;
+	}
 
 	public List<CryptoCoinHistoryPriceSubDTO> getPriceHistoryData() {
 		return priceHistoryData;
@@ -16,7 +36,8 @@ public class CryptoCoinHistoryPriceDTO {
 
 	@Override
 	public String toString() {
-		return "CryptoCoinHistoryPriceDTO [priceHistoryData=" + priceHistoryData + "]";
+		return "CryptoCoinHistoryPriceDTO [cryptoCoinTypeName=" + cryptoCoinTypeName + ", currencyName=" + currencyName
+				+ ", priceHistoryData=" + priceHistoryData + "]";
 	}
 
 }
