f53be283c851eb35c96db8074755edc01265a980
DavenChan <396767101@qq.com>
2020-11-18 10:42:40 +0800

    .

---
+++ src/main/java/clawing/pojo/dto/WawsawiwaComicsDTO.java
@@ -0,0 +1,20 @@
+package clawing.pojo.dto;
+
+public class WawsawiwaComicsDTO {
+
+	private String imgUrl;
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	@Override
+	public String toString() {
+		return "WawsawiwaComicsDTO [imgUrl=" + imgUrl + "]";
+	}
+
+}
