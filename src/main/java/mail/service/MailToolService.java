cd9ee9f04740a9bba4a417896af8452fdc5477aa
DavenChan <396767101@qq.com>
2021-01-04 17:52:55 +0800

    .

--- src/main/java/mail/service/MailToolService.java
+++ src/main/java/mail/service/MailToolService.java
@@ -3,6 +3,30 @@ package mail.service;
 import java.util.Properties;
 
 public class MailToolService {
+	
+	public Properties buildGmailSmtpSslProperties() {
+//		TODO
+		Properties properties = new Properties();
+		properties.setProperty("mail.smtp.auth", "true");
+		properties.setProperty("mail.smtp.host", "smtp.gmail.com");
+		properties.setProperty("mail.smtp.port", "465");
+		properties.setProperty("mail.smtp.starttls.enable", "true");
+		properties.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+
+		return properties;
+	}
+	
+	public Properties buildGmailImapSslProperties() {
+//		TODO
+		Properties properties = new Properties();
+		properties.setProperty("mail.imap.auth", "true");
+		properties.setProperty("mail.imap.host", "imap.gmail.com");
+		properties.setProperty("mail.imap.port", "993");
+		properties.setProperty("mail.imap.starttls.enable", "true");
+		properties.setProperty("mail.imap.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+
+		return properties;
+	}
 
 	public Properties buildSinaSmtpSslProperties() {
 		Properties properties = new Properties();
