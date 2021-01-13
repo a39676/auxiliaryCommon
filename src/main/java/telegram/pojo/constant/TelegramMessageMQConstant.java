55182ffee1b8f58a9a0486275a3ef7a49a4e4a60
DavenChan <396767101@qq.com>
2021-01-13 15:30:06 +0800

    .

--- src/main/java/telegram/pojo/constant/TelegramMessageMQConstant.java
+++ src/main/java/telegram/pojo/constant/TelegramMessageMQConstant.java
@@ -4,5 +4,4 @@ public class TelegramMessageMQConstant {
 
 	public static final String TELEGRAM_MSG_QUEUE = "TelegramMessageQueue";
 	
-	public static final String TELEGRAM_CRYPTO_COIN_NOTICE_SEND_RESULT_QUEUE = "TelegramCryptoCoinNoticeSendResultQueue";
 }
