634c82faa420276ad7b8e7b3ac29974270426117
DavenChan <396767101@qq.com>
2021-01-16 23:02:37 +0800

    .

---
+++ src/main/java/telegram/pojo/constant/TelegramBotType.java
@@ -0,0 +1,44 @@
+package telegram.pojo.constant;
+
+public enum TelegramBotType {
+	
+	BOT_1("bot1ID", 1),
+	BOT_2("bot2ID", 2),
+	CX_CALENDAR_NOTICE_BOT("CxCalendarNoticeBot", 3),
+	;
+	
+	private String name;
+	private Integer code;
+	
+	TelegramBotType(String name, Integer code) {
+		this.name = name;
+		this.code = code;
+	}
+	
+
+	public String getName() {
+		return name;
+	}
+
+	public Integer getCode() {
+		return code;
+	}
+
+	public static TelegramBotType getType(String typeName) {
+		for(TelegramBotType t : TelegramBotType.values()) {
+			if(t.getName().equals(typeName)) {
+				return t;
+			}
+		}
+		return null;
+	}
+	
+	public static TelegramBotType getType(Integer typeCode) {
+		for(TelegramBotType t : TelegramBotType.values()) {
+			if(t.getCode().equals(typeCode)) {
+				return t;
+			}
+		}
+		return null;
+	}
+}
--- src/main/java/telegram/pojo/constant/TelegramKeys.java
+++ src/main/java/telegram/pojo/constant/TelegramKeys.java
@@ -2,9 +2,6 @@ package telegram.pojo.constant;
 
 public class TelegramKeys {
 
-	public static final String BOT1_ID_KEY = "bot1ID";
-	public static final String CX_CALENDAR_NOTICE_BOT_ID_KEY = "CxCalendarNoticeBot";
-	
 	public static final String CHAT1_ID_KEY = "chat1ID";
 	public static final String CHAT2_ID_KEY = "chat2ID";
 	
