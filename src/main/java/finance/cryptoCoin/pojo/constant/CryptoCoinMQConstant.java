36ccb6745b72aeb4e64cc3504d5c287c7ee31433
DavenChan <396767101@qq.com>
2021-01-20 11:53:37 +0800

    .

--- src/main/java/finance/cryptoCoin/pojo/constant/CryptoCoinMQConstant.java
+++ src/main/java/finance/cryptoCoin/pojo/constant/CryptoCoinMQConstant.java
@@ -2,6 +2,5 @@ package finance.cryptoCoin.pojo.constant;
 
 public class CryptoCoinMQConstant {
 
-	public static final String CRYPTO_CONI_NEW_PRICE_DATA = "CryptoCoinNewPriceData";
 	public static final String CRYPTO_CONI_HISTORY_PRICE_DATA = "CryptoCoinHistoryPriceData";
 }
