8940d17bc3eff010a2de006c587f1c7101adad34
DavenChan <396767101@qq.com>
2021-02-19 11:53:38 +0800

    update CryptoCompareConstant.java

--- src/main/java/finance/cryptoCoin/pojo/constant/CryptoCompareConstant.java
+++ src/main/java/finance/cryptoCoin/pojo/constant/CryptoCompareConstant.java
@@ -5,6 +5,8 @@ public class CryptoCompareConstant {
 	public static final String SOCKET_LAST_ACTIVE_TIME_REDIS_KEY = "cryptoCompareLastActiveTime";
 	
 	public static final int SOCKET_INACTIVE_JUDGMENT_SECOND = 20;
+	
+	public static final int SOCKET_COLDDOWN_SECOND = 60;
 
 	public static final String PARAM_STORE_PATH = "cryptoCompare/socketConfig.json";
 }
