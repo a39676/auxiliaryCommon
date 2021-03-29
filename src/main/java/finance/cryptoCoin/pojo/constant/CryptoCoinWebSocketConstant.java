16be0f9b78c0f5a12fb4af797991b7b4144ae7b6
DavenChan <396767101@qq.com>
2021-03-29 21:08:13 +0800

    rename

---
+++ src/main/java/finance/cryptoCoin/pojo/constant/CryptoCoinWebSocketConstant.java
@@ -0,0 +1,15 @@
+package finance.cryptoCoin.pojo.constant;
+
+public class CryptoCoinWebSocketConstant {
+	
+	public static final String CRYPTO_COMPARE_SOCKET_LAST_ACTIVE_TIME_REDIS_KEY = "cryptoCompareWebSocketLastActiveTime";
+	public static final String BINANCE_SOCKET_LAST_ACTIVE_TIME_REDIS_KEY = "binanceWebSocketLastActiveTime";
+	
+	public static final int SOCKET_INACTIVE_JUDGMENT_SECOND = 20;
+	
+	public static final int SOCKET_COLDDOWN_SECOND = 60;
+
+	public static final String CRYPTO_COMPARE_PARAM_STORE_PATH = "cryptoCompare/socketConfig.json";
+	public static final String HUO_BI_PARAM_STORE_PATH = "huobi/socketConfig.json";
+	public static final String BINANCE_PARAM_STORE_PATH = "binance/socketConfig.json";
+}
