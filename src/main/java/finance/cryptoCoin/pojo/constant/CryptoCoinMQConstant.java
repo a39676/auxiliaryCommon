a512ab1a554afac048e61591f7bff4902e2b3ddc
DavenChan <396767101@qq.com>
2021-03-31 19:09:32 +0800

    .

--- src/main/java/finance/cryptoCoin/pojo/constant/CryptoCoinMQConstant.java
+++ src/main/java/finance/cryptoCoin/pojo/constant/CryptoCoinMQConstant.java
@@ -2,11 +2,8 @@ package finance.cryptoCoin.pojo.constant;
 
 public class CryptoCoinMQConstant {
 
-//	public static final String CRYPTO_CONI_HISTORY_PRICE_DATA = "CryptoCoinHistoryPriceData";
+	public static final String CRYPTO_COIN_DAILY_DATA_QUERY = "CryptoCoinDailyDataQuery";
 	
-	/*
-	 * TODO 未变更对应MQ
-	 */
 	public static final String CRYPTO_COIN_MINUTE_DATA = "CryptoCoinMinuteData";
 	public static final String CRYPTO_COIN_DAILY_DATA = "CryptoCoinDailyData";
 	
---
+++ src/main/java/finance/cryptoCoin/pojo/dto/CryptoCoinDailyDataQueryDTO.java
@@ -0,0 +1,24 @@
+package finance.cryptoCoin.pojo.dto;
+
+public class CryptoCoinDailyDataQueryDTO {
+
+	private String coinName;
+	private Integer counting;
+
+	public String getCoinName() {
+		return coinName;
+	}
+
+	public void setCoinName(String coinName) {
+		this.coinName = coinName;
+	}
+
+	public Integer getCounting() {
+		return counting;
+	}
+
+	public void setCounting(Integer counting) {
+		this.counting = counting;
+	}
+
+}
