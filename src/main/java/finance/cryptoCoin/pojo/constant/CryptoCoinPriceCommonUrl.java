933e5080f820363969e103dd71481bcccfbe9edc
DavenChan <396767101@qq.com>
2021-04-17 17:44:57 +0800

    .

--- src/main/java/finance/cryptoCoin/pojo/constant/CryptoCoinPriceCommonUrl.java
+++ src/main/java/finance/cryptoCoin/pojo/constant/CryptoCoinPriceCommonUrl.java
@@ -8,4 +8,6 @@ public class CryptoCoinPriceCommonUrl {
 	
 	public static final String GET_SUBSCRIPTION_CATALOG = "/getSubscriptionCatalog";
 	
+	public static final String GET_LOW_PRICE_SUBSCRIPTION_CATALOG = "/getLowPriceSubscriptionCatalog";
+	
 }
