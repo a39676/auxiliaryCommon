da6fd7bb4972cbbf4d7638c99ddba39c6ed91387
DavenChan <396767101@qq.com>
2021-04-22 16:44:55 +0800

    .

--- src/main/java/telegram/pojo/dto/TelegramMessageDTO.java
+++ src/main/java/telegram/pojo/dto/TelegramMessageDTO.java
@@ -1,38 +1,38 @@
-//package telegram.pojo.dto;
-//
-//public class TelegramMessageDTO {
-//
-//	private String msg;
-//	private Long id;
-//	private String botName;
-//
-//	public String getBotName() {
-//		return botName;
-//	}
-//
-//	public void setBotName(String botName) {
-//		this.botName = botName;
-//	}
-//
-//	public String getMsg() {
-//		return msg;
-//	}
-//
-//	public void setMsg(String msg) {
-//		this.msg = msg;
-//	}
-//
-//	public Long getId() {
-//		return id;
-//	}
-//
-//	public void setId(Long id) {
-//		this.id = id;
-//	}
-//
-//	@Override
-//	public String toString() {
-//		return "TelegramMessageDTO [msg=" + msg + ", id=" + id + ", botName=" + botName + "]";
-//	}
-//
-//}
+package telegram.pojo.dto;
+
+public class TelegramMessageDTO {
+
+	private String msg;
+	private Long id;
+	private String botName;
+
+	public String getBotName() {
+		return botName;
+	}
+
+	public void setBotName(String botName) {
+		this.botName = botName;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "TelegramMessageDTO [msg=" + msg + ", id=" + id + ", botName=" + botName + "]";
+	}
+
+}
