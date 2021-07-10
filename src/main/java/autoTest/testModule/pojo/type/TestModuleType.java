e4d2343a180c0a48619ab95a180ec42e8527f3cb
DavenChan <396767101@qq.com>
2021-07-10 23:07:56 +0800

    .

--- src/main/java/autoTest/testModule/pojo/type/TestModuleType.java
+++ src/main/java/autoTest/testModule/pojo/type/TestModuleType.java
@@ -2,17 +2,11 @@ package autoTest.testModule.pojo.type;
 
 public enum TestModuleType {
 
-	/** badJoke */
-	badJoke(2L, "badJoke"),
-
 	/** ATDemo */
 	ATDemo(3L, "ATDemo"),
 	
 	/** scheduleClawing */
 	scheduleClawing(4L, "scheduleClawing"),
-	
-	/** lottery */
-	lottery(5L, "lottery"),
 	
 	/** collecting */
 	collecting(6L, "collecting"),
