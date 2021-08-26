92f6119348f356352b7a3063a4dcee3fe7d3bcce
DavenChan <q39676@gmail.com>
2021-08-26 16:15:15 +0800

    .

--- src/main/java/finance/cryptoCoin/pojo/constant/CryptoCoinMQConstant.java
+++ src/main/java/finance/cryptoCoin/pojo/constant/CryptoCoinMQConstant.java
@@ -2,8 +2,6 @@ package finance.cryptoCoin.pojo.constant;
 
 public class CryptoCoinMQConstant {
 
-	public static final String CRYPTO_COIN_DAILY_DATA_QUERY = "CryptoCoinDailyDataQuery";
-
 	public static final String CRYPTO_COIN_DAILY_DATA = "CryptoCoinDailyData";
 
 	public static final String CRYPTO_COIN_PRICE_CACHE_QUEUE = "CryptoCoinPriceCacheQueue";
