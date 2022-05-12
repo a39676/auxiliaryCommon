fb5baed7a7b6f879c838ca7cbc456f3359ce3b27
DavenChan <q39676@gmail.com>
2022-05-13 00:46:08 +0800

    Update

---
+++ src/main/java/tool/pojo/type/InternationalityType.java
@@ -0,0 +1,228 @@
+package tool.pojo.type;
+
+public enum InternationalityType {
+
+	CN(86, "China", "中国"),
+	HK(852, "Hongkong", "香港"),
+	MO(853, "Macao", "澳门"),
+	TW(886, "Taiwan", "台湾"),
+	US(1, "United States of America", "美国"),
+	CA(1, "Canada", "加拿大"),
+	GB(44, "United Kiongdom", "英国"),
+	MY(60, "Malaysia", "马来西亚"),
+	IN(91, "India", "印度"),
+	SG(65, "Singapore", "新加坡"),
+	AO(244, "Angola", "安哥拉"),
+	AF(93, "Afghanistan", "阿富汗"),
+	AL(355, "Albania", "阿尔巴尼亚"),
+	DZ(213, "Algeria", "阿尔及利亚"),
+	AD(376, "Andorra", "安道尔共和国"),
+	AI(1264, "Anguilla", "安圭拉岛"),
+	AG(1268, "Antigua and Barbuda", "安提瓜和巴布达"),
+    AR(54, "Argentina", "阿根廷"),
+    AM(374, "Armenia", "亚美尼亚"),
+    AU(61, "Australia", "澳大利亚"),
+    AT(43, "Austria", "奥地利"),
+    AZ(994, "Azerbaijan", "阿塞拜疆"),
+    BS(1242, "Bahamas", "巴哈马"),
+    BH(973, "Bahrain", "巴林"),
+    BD(880, "Bangladesh", "孟加拉国"),
+    BB(1246, "Barbados", "巴巴多斯"),
+    BY(375, "Belarus", "白俄罗斯"),
+    BE(32, "Belgium", "比利时"),
+    BZ(501, "Belize", "伯利兹"),
+    BJ(229, "Benin", "贝宁"),
+    BM(1441, "Bermuda Is.", "百慕大群岛"),
+    BO(591, "Bolivia", "玻利维亚"),
+    BW(267, "Botswana", "博茨瓦纳"),
+    BR(55, "Brazil", "巴西"),
+    BN(673, "Brunei", "文莱"),
+    BG(359, "Bulgaria", "保加利亚"),
+    BF(226, "Burkina-faso", "布基纳法索"),
+    MM(95, "Burma", "缅甸"),
+    BI(257, "Burundi", "布隆迪"),
+    CM(237, "Cameroon", "喀麦隆"),
+    CF(236, "Central African Republic", "中非共和国"),
+    TD(235, "Chad", "乍得"),
+    CL(56, "Chile", "智利"),
+    CO(57, "Colombia", "哥伦比亚"),
+    CG(242, "Congo", "刚果"),
+    CK(682, "Cook Is.", "库克群岛"),
+    CR(506, "Costa Rica", "哥斯达黎加"),
+    CU(53, "Cuba", "古巴"),
+    CY(357, "Cyprus", "塞浦路斯"),
+    CZ(420, "Czech Republic", "捷克"),
+    DK(45, "Denmark", "丹麦"),
+    DJ(253, "Djibouti", "吉布提"),
+    DO(1890, "Dominica Rep.", "多米尼加共和国"),
+    EC(593, "Ecuador", "厄瓜多尔"),
+    EG(20, "Egypt", "埃及"),
+    SV(503, "EI Salvador", "萨尔瓦多"),
+    EE(372, "Estonia", "爱沙尼亚"),
+    ET(251, "Ethiopia", "埃塞俄比亚"),
+    FJ(679, "Fiji", "斐济"),
+    FI(358, "Finland", "芬兰"),
+    FR(33, "France", "法国"),
+    GF(594, "French Guiana", "法属圭亚那"),
+    GA(241, "Gabon", "加蓬"),
+    GM(220, "Gambia", "冈比亚"),
+    GE(995, "Georgia", "格鲁吉亚"),
+    DE(49, "Germany", "德国"),
+    GH(233, "Ghana", "加纳"),
+    GI(350, "Gibraltar", "直布罗陀"),
+    GR(30, "Greece", "希腊"),
+    GD(1809, "Grenada", "格林纳达"),
+    GU(1671, "Guam", "关岛"),
+    GT(502, "Guatemala", "危地马拉"),
+    GN(224, "Guinea", "几内亚"),
+    GY(592, "Guyana", "圭亚那"),
+    HT(509, "Haiti", "海地"),
+    HN(504, "Honduras", "洪都拉斯"),
+    HU(36, "Hungary", "匈牙利"),
+    IS(354, "Iceland", "冰岛"),
+    ID(62, "Indonesia", "印度尼西亚"),
+    IR(98, "Iran", "伊朗"),
+    IQ(964, "Iraq", "伊拉克"),
+    IE(353, "Ireland", "爱尔兰"),
+    IL(972, "Israel", "以色列"),
+    IT(39, "Italy", "意大利"),
+    JM(1876, "Jamaica", "牙买加"),
+    JP(81, "Japan", "日本"),
+    JO(962, "Jordan", "约旦"),
+    KH(855, "Kampuchea (Cambodia )", "柬埔寨"),
+    KZ(327, "Kazakstan", "哈萨克斯坦"),
+    KE(254, "Kenya", "肯尼亚"),
+    KR(82, "Korea", "韩国"),
+    KW(965, "Kuwait", "科威特"),
+    KG(331, "Kyrgyzstan", "吉尔吉斯坦"),
+    LA(856, "Laos", "老挝"),
+    LV(371, "Latvia", "拉脱维亚"),
+    LB(961, "Lebanon", "黎巴嫩"),
+    LS(266, "Lesotho", "莱索托"),
+    LR(231, "Liberia", "利比里亚"),
+    LY(218, "Libya", "利比亚"),
+    LI(423, "Liechtenstein", "列支敦士登"),
+    LT(370, "Lithuania", "立陶宛"),
+    LU(352, "Luxembourg", "卢森堡"),
+    MG(261, "Madagascar", "马达加斯加"),
+    MW(265, "Malawi", "马拉维"),
+    MV(960, "Maldives", "马尔代夫"),
+    ML(223, "Mali", "马里"),
+    MT(356, "Malta", "马耳他"),
+    MU(230, "Mauritius", "毛里求斯"),
+    MX(52, "Mexico", "墨西哥"),
+    MD(373, "Moldova, Republic of", "摩尔多瓦"),
+    MC(377, "Monaco", "摩纳哥"),
+    MN(976, "Mongolia", "蒙古"),
+    MS(1664, "Montserrat Is", "蒙特塞拉特岛"),
+    MA(212, "Morocco", "摩洛哥"),
+    MZ(258, "Mozambique", "莫桑比克"),
+    NA(264, "Namibia", "纳米比亚"),
+    NR(674, "Nauru", "瑙鲁"),
+    NP(977, "Nepal", "尼泊尔"),
+    NL(31, "Netherlands", "荷兰"),
+    NZ(64, "New Zealand", "新西兰"),
+    NI(505, "Nicaragua", "尼加拉瓜"),
+    NE(227, "Niger", "尼日尔"),
+    NG(234, "Nigeria", "尼日利亚"),
+    KP(850, "North Korea", "朝鲜"),
+    NO(47, "Norway", "挪威"),
+    OM(968, "Oman", "阿曼"),
+    PK(92, "Pakistan", "巴基斯坦"),
+    PA(507, "Panama", "巴拿马"),
+    PG(675, "Papua New Cuinea", "巴布亚新几内亚"),
+    PY(595, "Paraguay", "巴拉圭"),
+    PE(51, "Peru", "秘鲁"),
+    PH(63, "Philippines", "菲律宾"),
+    PL(48, "Poland", "波兰"),
+    PF(689, "French Polynesia", "法属玻利尼西亚"),
+    PT(351, "Portugal", "葡萄牙"),
+    PR(1787, "Puerto Rico", "波多黎各"),
+    QA(974, "Qatar", "卡塔尔"),
+    RO(40, "Romania", "罗马尼亚"),
+    RU(7, "Russia", "俄罗斯"),
+    SM(378, "San Marino", "圣马力诺"),
+    ST(239, "Sao Tome and Principe", "圣多美和普林西比"),
+    SA(966, "Saudi Arabia", "沙特阿拉伯"),
+    SN(221, "Senegal", "塞内加尔"),
+    SC(248, "Seychelles", "塞舌尔"),
+    SL(232, "Sierra Leone", "塞拉利昂"),
+    
+    SK(421, "Slovakia", "斯洛伐克"),
+    SI(386, "Slovenia", "斯洛文尼亚"),
+    SB(677, "Solomon Is", "所罗门群岛"),
+    SO(252, "Somali", "索马里"),
+    ZA(27, "South Africa", "南非"),
+    ES(34, "Spain", "西班牙"),
+    LK(94, "Sri Lanka", "斯里兰卡"),
+    LC(1758, "St.Lucia", "圣卢西亚"),
+    VC(1784, "St.Vincent", "圣文森特"),
+    SD(249, "Sudan", "苏丹"),
+    SR(597, "Suriname", "苏里南"),
+    SZ(268, "Swaziland", "斯威士兰"),
+    SE(46, "Sweden", "瑞典"),
+    CH(41, "Switzerland", "瑞士"),
+    SY(963, "Syria", "叙利亚"),
+    TJ(992, "Tajikstan", "塔吉克斯坦"),
+    TZ(255, "Tanzania", "坦桑尼亚"),
+    TH(66, "Thailand", "泰国"),
+    TG(228, "Togo", "多哥"),
+    TO(676, "Tonga", "汤加"),
+    TT(1809, "Trinidad and Tobago", "特立尼达和多巴哥"),
+    TN(216, "Tunisia", "突尼斯"),
+    TR(90, "Turkey", "土耳其"),
+    TM(993, "Turkmenistan", "土库曼斯坦"),
+    UG(256, "Uganda", "乌干达"),
+    UA(380, "Ukraine", "乌克兰"),
+    AE(971, "United Arab Emirates", "阿拉伯联合酋长国"),
+    UY(598, "Uruguay", "乌拉圭"),
+    UZ(233, "Uzbekistan", "乌兹别克斯坦"),
+    VE(58, "Venezuela", "委内瑞拉"),
+    VN(84, "Vietnam", "越南"),
+    YE(967, "Yemen", "也门"),
+    YU(381, "Yugoslavia", "南斯拉夫"),
+    ZW(263, "Zimbabwe", "津巴布韦"),
+    ZR(243, "Zaire", "扎伊尔"),
+    ZM(260, "Zambia", "赞比亚"),
+	;
+
+	private Integer code;
+	private String enName;
+	private String cnName;
+
+	InternationalityType(Integer code, String enName, String cnName) {
+		this.code = code;
+		this.enName = enName;
+		this.cnName = cnName;
+	}
+
+	public Integer getCode() {
+		return code;
+	}
+
+	public String getEnName() {
+		return enName;
+	}
+
+	public String getCnName() {
+		return cnName;
+	}
+
+	public static InternationalityType getType(String enName) {
+		for(InternationalityType t : InternationalityType.values()) {
+			if(t.getEnName().equals(enName)) {
+				return t;
+			}
+		}
+		return null;
+	}
+
+	public static InternationalityType getType(Integer typeCode) {
+		for(InternationalityType t : InternationalityType.values()) {
+			if(t.getCode().equals(typeCode)) {
+				return t;
+			}
+		}
+		return null;
+	}
+}
