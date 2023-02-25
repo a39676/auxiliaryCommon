46678b557d35f99739a748e8d690e43085b83c14
DavenChan <demo@demo.com>
2023-02-25 16:42:00 +0800

    Update

--- src/main/java/image/pojo/constant/ImageInteractionUrl.java
+++ src/main/java/image/pojo/constant/ImageInteractionUrl.java
@@ -2,9 +2,9 @@ package image.pojo.constant;
 
 public class ImageInteractionUrl {
 
-	public static final String root = "/imageInteraction";
+	public static final String ROOT = "/imageInteraction";
 	
-	public static final String uploadImageToCloudinary = "/uploadImageToCloudinary";
+	public static final String UPLOAD_IMAGE_TO_CLOUDINARY = "/uploadImageToCloudinary";
 	
 	public static final String IMAGE_SAVING_FROM_BBT = "/imageSavingFromBBT";
 	
