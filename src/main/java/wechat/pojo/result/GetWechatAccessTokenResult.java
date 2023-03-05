a71a2e22228efaa55639b71f3268c0c420351700
DavenChan <demo@demo.com>
2023-03-06 07:25:26 +0800

    .

---
+++ src/main/java/wechat/pojo/result/GetWechatAccessTokenResult.java
@@ -0,0 +1,25 @@
+package wechat.pojo.result;
+
+import auxiliaryCommon.pojo.result.CommonResult;
+
+public class GetWechatAccessTokenResult extends CommonResult {
+
+	private String accessToken;
+
+	public String getAccessToken() {
+		return accessToken;
+	}
+
+	public void setAccessToken(String accessToken) {
+		this.accessToken = accessToken;
+	}
+
+	@Override
+	public String toString() {
+		return "GetWechatAccessTokenResult [accessToken=" + accessToken + ", getCode()=" + getCode() + ", getResult()="
+				+ getResult() + ", getMessage()=" + getMessage() + ", isSuccess()=" + isSuccess() + ", isFail()="
+				+ isFail() + ", toString()=" + super.toString() + ", getClass()=" + getClass() + ", hashCode()="
+				+ hashCode() + "]";
+	}
+
+}
