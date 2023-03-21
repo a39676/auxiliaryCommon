b9096bd711ecaf50d077d9113c3b28b03c81d7e8
DavenChan <demo@demo.com>
2023-03-21 12:08:44 +0800

    Url rename

--- src/main/java/wechatSdk/pojo/constant/WechatSdkUrlConstant.java
+++ src/main/java/wechatSdk/pojo/constant/WechatSdkUrlConstant.java
@@ -2,12 +2,11 @@ package wechatSdk.pojo.constant;
 
 public class WechatSdkUrlConstant {
 
-	public static final String WECHAT_MANAGER = "/wechatManager";
+	public static final String ROOT = "/wechatManager";
 
 	public static final String UPDATE_ACCESS_TOKEN = "/updateAccessToken";
 
 	public static final String SYSTEM_OPTION_REFRESH = "/refreshSystemOption";
-	public static final String WECHAT_OPTION_REFRESH = "/refreshWechatOption";
 	public static final String GET_WECHAT_SDK_WECHAT_OPTION = "/getWechatSdkWechatOption";
 	public static final String UPDATE_WECHAT_ACCESS_TOKEN = "/udpateWechatAccessToken";
 	public static final String GET_WECHAT_ACCESS_TOKEN = "/getWechatAccessToken";
