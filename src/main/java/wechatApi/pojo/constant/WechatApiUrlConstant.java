9d16c5a29b0ccc11317efe2535ec2ac6597097a5
DavenChan <demo@demo.com>
2023-03-29 14:34:33 +0800

    Update
    Add Wechat create QR code url

--- src/main/java/wechatApi/pojo/constant/WechatApiUrlConstant.java
+++ src/main/java/wechatApi/pojo/constant/WechatApiUrlConstant.java
@@ -7,5 +7,5 @@ public class WechatApiUrlConstant {
 	public static final String GET_UNION_ID_ACCESS_TOKEN_BY_CODE = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=%s&secret=%s&code=%s&grant_type=authorization_code";
 	public static final String GET_USER_INFO_BY_OPEN_ID = "https://api.weixin.qq.com/cgi-bin/user/info?access_token=%s&openid=%s&lang=zh_CN";
 	public static final String GET_USERINFO_BY_UNION_ACCESS_TOKEN = "https://api.weixin.qq.com/sns/userinfo?access_token=%s&openid=%s&lang=zh_CN";
-	
+	public static final String CREATE_LONGLIVE_QR_CODE = "https://api.weixin.qq.com/cgi-bin/qrcode/create?access_token=";
 }
