c2f91a1b141c039b26378952c8c2b0fba7452012
DavenChan <19781485+a39676@users.noreplay.github.com>
2023-04-08 01:11:19 +0800

    .

--- src/main/java/wechatApi/pojo/constant/WechatApiUrlConstant.java
+++ src/main/java/wechatApi/pojo/constant/WechatApiUrlConstant.java
@@ -8,4 +8,5 @@ public class WechatApiUrlConstant {
 	public static final String GET_USER_INFO_BY_OPEN_ID = "https://api.weixin.qq.com/cgi-bin/user/info?access_token=%s&openid=%s&lang=zh_CN";
 	public static final String GET_USERINFO_BY_UNION_ACCESS_TOKEN = "https://api.weixin.qq.com/sns/userinfo?access_token=%s&openid=%s&lang=zh_CN";
 	public static final String CREATE_LONGLIVE_QR_CODE = "https://api.weixin.qq.com/cgi-bin/qrcode/create?access_token=";
+	public static final String GET_QR_CODE_WITH_TICKET = "https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=";
 }
