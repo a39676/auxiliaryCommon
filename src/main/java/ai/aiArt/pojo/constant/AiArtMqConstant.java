e17b7ece1a980e64fbbd4f12b35355d9ab3ef435
DavenChan <19781485+a39676@users.noreplay.github.com>
2023-05-07 15:55:39 +0800

    Update

--- src/main/java/ai/aiArt/pojo/constant/AiArtMqConstant.java
+++ src/main/java/ai/aiArt/pojo/constant/AiArtMqConstant.java
@@ -6,4 +6,5 @@ public class AiArtMqConstant {
 	public static final String AI_ART_IMAGE_TO_IMAGE = "aiArtImageToImage";
 	public static final String AI_ART_IMAGE_FEEDBACK = "aiArtImageFeedback";
 	public static final String AI_ART_AUTOMATIC_HEART_BEAT = "automaticHeartBeat";
+	public static final String AI_ART_JOB_QUEUE_SETTING = "aiArtJobQueueSetting";
 }
