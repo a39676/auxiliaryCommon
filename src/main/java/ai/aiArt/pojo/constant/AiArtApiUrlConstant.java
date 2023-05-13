3f0b3d2a110f22784685552da950cd8767202d83
DavenChan <19781485+a39676@users.noreplay.github.com>
2023-05-13 14:52:04 +0800

    .

--- src/main/java/ai/aiArt/pojo/constant/AiArtApiUrlConstant.java
+++ src/main/java/ai/aiArt/pojo/constant/AiArtApiUrlConstant.java
@@ -8,5 +8,7 @@ public class AiArtApiUrlConstant {
 	public static final String GET_MODEL_LIST = "/getModelList";
 	public static final String GET_UPSCALER_LIST = "/getUpscalerList";
 	public static final String GET_SAMPLER_LIST = "/getSamplerList";
-
+	public static final String RECEIVE_IMAGE_JOB_RESULT = "/receiveImgJobResultForApi";
+	public static final String HEART_BEAT = "/heartBeat";
+	
 }
