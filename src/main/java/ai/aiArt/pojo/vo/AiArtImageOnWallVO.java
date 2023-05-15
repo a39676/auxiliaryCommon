bf451b204f01fa3bb32664996ec83a722b19f3f7
DavenChan <19781485+a39676@users.noreplay.github.com>
2023-05-15 12:41:36 +0800

    Update

--- src/main/java/ai/aiArt/pojo/vo/AiArtImageOnWallVO.java
+++ src/main/java/ai/aiArt/pojo/vo/AiArtImageOnWallVO.java
@@ -4,6 +4,8 @@ public class AiArtImageOnWallVO {
 
 	private String jobPk;
 	private String imgPk;
+	private Long jobId;
+	private Long imgId;
 
 	public String getJobPk() {
 		return jobPk;
@@ -21,9 +23,26 @@ public class AiArtImageOnWallVO {
 		this.imgPk = imgPk;
 	}
 
+	public Long getJobId() {
+		return jobId;
+	}
+
+	public void setJobId(Long jobId) {
+		this.jobId = jobId;
+	}
+
+	public Long getImgId() {
+		return imgId;
+	}
+
+	public void setImgId(Long imgId) {
+		this.imgId = imgId;
+	}
+
 	@Override
 	public String toString() {
-		return "AiArtImageOnWallVo [jobPk=" + jobPk + ", imgPk=" + imgPk + "]";
+		return "AiArtImageOnWallVO [jobPk=" + jobPk + ", imgPk=" + imgPk + ", jobId=" + jobId + ", imgId=" + imgId
+				+ "]";
 	}
 
 }
