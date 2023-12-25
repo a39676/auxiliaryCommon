ebb2ec86debb6bcca7e3e132203469e1027bd7b8
DavenChan <19781485+a39676@users.noreplay.github.com>
2023-12-25 11:05:23 +0800

    .

--- src/main/java/tool/pojo/constant/BbtInteractionUrl.java
+++ src/main/java/tool/pojo/constant/BbtInteractionUrl.java
@@ -5,5 +5,6 @@ public class BbtInteractionUrl {
 	public static final String ROOT = "/bbt";
 	public static final String TEXT_MESSAGE_FORWARD = "/textMessageForward";
 	public static final String RECEIVE_CURRENCY_EXCHANGE_RATE_DAILY_DATA = "/receiveCurrencyExchangeRateDailyData";
-
+	public static final String BBT_HEART_BEAT = "/bbtHeartBeat";
+	
 }
