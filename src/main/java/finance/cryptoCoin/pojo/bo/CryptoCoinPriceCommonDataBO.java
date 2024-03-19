33f84983b17e73527e75c5213e597974e7dde4a7
Daven <19781485+a39676@users.noreply.github.com>
2024-03-19 16:08:10 +0800

    Update

--- src/main/java/finance/cryptoCoin/pojo/bo/CryptoCoinPriceCommonDataBO.java
+++ src/main/java/finance/cryptoCoin/pojo/bo/CryptoCoinPriceCommonDataBO.java
@@ -4,8 +4,10 @@ import finance.common.pojo.bo.KLineCommonDataBO;
 
 public class CryptoCoinPriceCommonDataBO extends KLineCommonDataBO {
 
+	@Deprecated
 	private String coinType;
 	private Integer currencyType;
+	private String symbol;
 
 	public String getCoinType() {
 		return coinType;
@@ -23,14 +25,23 @@ public class CryptoCoinPriceCommonDataBO extends KLineCommonDataBO {
 		this.currencyType = currencyType;
 	}
 
+	public String getSymbol() {
+		return symbol;
+	}
+
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+
 	@Override
 	public String toString() {
-		return "CryptoCoinPriceCommonDataBO [coinType=" + coinType + ", currencyType=" + currencyType + ", getId()="
-				+ getId() + ", getStartPrice()=" + getStartPrice() + ", getEndPrice()=" + getEndPrice()
-				+ ", getHighPrice()=" + getHighPrice() + ", getLowPrice()=" + getLowPrice() + ", getVolume()="
-				+ getVolume() + ", getStartTime()=" + getStartTime() + ", getEndTime()=" + getEndTime()
-				+ ", getCreateTime()=" + getCreateTime() + ", getIsDelete()=" + getIsDelete() + ", getInterval()="
-				+ getInterval() + "]";
+		return "CryptoCoinPriceCommonDataBO [coinType=" + coinType + ", currencyType=" + currencyType + ", symbol="
+				+ symbol + ", getId()=" + getId() + ", getStartPrice()=" + getStartPrice() + ", getEndPrice()="
+				+ getEndPrice() + ", getHighPrice()=" + getHighPrice() + ", getLowPrice()=" + getLowPrice()
+				+ ", getVolume()=" + getVolume() + ", getStartTime()=" + getStartTime() + ", getEndTime()="
+				+ getEndTime() + ", getCreateTime()=" + getCreateTime() + ", getIsDelete()=" + getIsDelete()
+				+ ", getInterval()=" + getInterval() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+				+ ", toString()=" + super.toString() + "]";
 	}
 
 }
--- src/main/java/tool/pojo/constant/BbtInteractionUrl.java
+++ src/main/java/tool/pojo/constant/BbtInteractionUrl.java
@@ -7,5 +7,6 @@ public class BbtInteractionUrl {
 	public static final String RECEIVE_CURRENCY_EXCHANGE_RATE_DAILY_DATA = "/receiveCurrencyExchangeRateDailyData";
 	public static final String BBT_HEART_BEAT = "/bbtHeartBeat";
 	public static final String CN_STOCK_MARKET_DATA = "/cnStockMarketData";
-	
+	public static final String GET_CRYPTO_COIN_OPTION = "/getCryptoCoinOption";
+
 }
