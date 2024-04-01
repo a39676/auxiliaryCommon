1cd48621ae3deadd68e041b65ca1fa48035b6a4c
Daven <19781485+a39676@users.noreply.github.com>
2024-04-01 16:43:53 +0800

    Update

--- src/main/java/tool/pojo/constant/CxBbtInteractionUrl.java
+++ src/main/java/tool/pojo/constant/CxBbtInteractionUrl.java
@@ -1,12 +1,13 @@
 package tool.pojo.constant;
 
-public class BbtInteractionUrl {
+public class CxBbtInteractionUrl {
 
-	public static final String ROOT = "/bbt";
+	public static final String ROOT = "/cxbbt";
 	public static final String TEXT_MESSAGE_FORWARD = "/textMessageForward";
 	public static final String RECEIVE_CURRENCY_EXCHANGE_RATE_DAILY_DATA = "/receiveCurrencyExchangeRateDailyData";
 	public static final String BBT_HEART_BEAT = "/bbtHeartBeat";
 	public static final String CN_STOCK_MARKET_DATA = "/cnStockMarketData";
 	public static final String GET_CRYPTO_COIN_OPTION = "/getCryptoCoinOption";
-
+	public static final String WORKER_PING = "/ping";
+	public static final String MAKR_SURE_ALIVE_WITH_CTHULHU = "/workerAlive";
 }
