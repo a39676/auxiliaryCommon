a5c3930bf486fe82f07ebc204874a969b7d27fb0
Daven <19781485+a39676@users.noreply.github.com>
2024-04-10 15:17:43 +0800

    Update

--- src/main/java/auxiliaryCommon/pojo/constant/ServiceMQConstant.java
+++ src/main/java/auxiliaryCommon/pojo/constant/ServiceMQConstant.java
@@ -1,7 +1,8 @@
 package auxiliaryCommon.pojo.constant;
 
 public class ServiceMQConstant {
-	
+
 	public static final String BBT_SEND_MESSAGE_QUEUE = "BBTMessageQueue";
-	
+	public static final String HEART_BEAT = "BbtHeartBeat";
+
 }
