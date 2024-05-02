44685eef19bb71b2ecc3dbd1798da2d2e9d7b7ae
Daven <19781485+a39676@users.noreply.github.com>
2024-05-02 18:13:13 +0800

    Update

--- src/main/java/finance/common/pojo/type/IntervalType.java
+++ src/main/java/finance/common/pojo/type/IntervalType.java
@@ -1,18 +1,47 @@
 package finance.common.pojo.type;
 
+import auxiliaryCommon.pojo.type.TimeUnitType;
+
 public enum IntervalType {
 
-	MINUTE_1("1m"), MINUTE_3("3m"), MINUTE_5("5m"), MINUTE_15("15m"), MINUTE_30("30m"), HOUR_1("1h"), HOUR_2("2h"),
-	HOUR_4("4h"), HOUR_6("6h"), HOUR_8("8h"), HOUR_12("12h"), DAY_1("1d"), DAY_3("3d"), WEEK("1w"), MONTH("1M"),;
+	MINUTE_1("1m", 1, TimeUnitType.MINUTE),
+	MINUTE_3("3m", 3, TimeUnitType.MINUTE),
+	MINUTE_5("5m", 5, TimeUnitType.MINUTE),
+	MINUTE_15("15m", 15, TimeUnitType.MINUTE),
+	MINUTE_30("30m", 30, TimeUnitType.MINUTE),
+	HOUR_1("1h", 1, TimeUnitType.HOUR),
+	HOUR_2("2h", 2, TimeUnitType.HOUR),
+	HOUR_4("4h", 4, TimeUnitType.HOUR),
+	HOUR_6("6h", 6, TimeUnitType.HOUR),
+	HOUR_8("8h", 8, TimeUnitType.HOUR),
+	HOUR_12("12h", 12, TimeUnitType.HOUR),
+	DAY_1("1d", 1, TimeUnitType.DAY),
+	DAY_3("3d", 3, TimeUnitType.DAY),
+	WEEK("1w", 1, TimeUnitType.WEEK),
+	MONTH("1M", 1, TimeUnitType.MONTH),
+	;
+
 
 	private String name;
+	private Integer range;
+	private TimeUnitType timeUnit;
 
-	IntervalType(String name) {
+	IntervalType(String name, Integer range, TimeUnitType timeUnit) {
 		this.name = name;
+		this.range = range;
+		this.timeUnit = timeUnit;
 	}
 
 	public String getName() {
 		return name;
+	}
+
+	public Integer getRange() {
+		return range;
+	}
+
+	public TimeUnitType getTimeUnit() {
+		return timeUnit;
 	}
 
 	public static IntervalType getType(String typeName) {
