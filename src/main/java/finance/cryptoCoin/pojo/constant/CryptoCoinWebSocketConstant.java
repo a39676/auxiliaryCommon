5d2837fe37bec656b25cf2e250f253a86908bfa4
Daven <19781485+a39676@users.noreply.github.com>
2024-05-12 16:48:32 +0800

    .

--- src/main/java/finance/cryptoCoin/pojo/constant/CryptoCoinWebSocketConstant.java
+++ src/main/java/finance/cryptoCoin/pojo/constant/CryptoCoinWebSocketConstant.java
@@ -2,10 +2,6 @@ package finance.cryptoCoin.pojo.constant;
 
 public class CryptoCoinWebSocketConstant {
 	
-	public static final int CRYPTO_COMPARE_SOCKET_INACTIVE_JUDGMENT_SECOND = 20;
-	public static final int BINANCE_SOCKET_INACTIVE_JUDGMENT_SECOND = 10;
-	public static final int GATE_IO_SOCKET_INACTIVE_JUDGMENT_SECOND = 10;
-	
 	public static final int SOCKET_COLDDOWN_SECOND = 60;
 
 }
