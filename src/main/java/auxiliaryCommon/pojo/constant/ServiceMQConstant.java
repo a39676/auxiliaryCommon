543b1768477f59c4e739ff08f03ff70823d41199
Daven <19781485+a39676@users.noreply.github.com>
2024-05-21 11:55:34 +0800

    .

--- src/main/java/auxiliaryCommon/pojo/constant/ServiceMQConstant.java
+++ src/main/java/auxiliaryCommon/pojo/constant/ServiceMQConstant.java
@@ -3,8 +3,9 @@ package auxiliaryCommon.pojo.constant;
 public class ServiceMQConstant {
 
 	public static final String BBT_SEND_MESSAGE_QUEUE = "BBTMessageQueue";
-	public static final String NORMAL_MESSAGE_QUEUE = "normalMsg";
-	public static final String MID_SMALL_CAPITALIZATION_CRYPTO_COIN_MSG = "midSmallCapCryptoCoinMsg";
+	public static final String CRYPTO_COIN_NORMAL_MESSAGE_QUEUE = "CryptoCoinNormalMsg";
+	public static final String CRYPTO_COIN_MID_SMALL_CAPITALIZATION_MSG = "CryptoCoinMidSmallCapMsg";
+	public static final String CRYPTO_COIN_PRICE_RANGE_MSG = "CryptoCoinPriceRangeMsg";
 	public static final String HEART_BEAT = "HeartBeat";
 
 }
