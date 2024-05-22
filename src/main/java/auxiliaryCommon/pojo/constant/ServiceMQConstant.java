c12ea970920011d2e926cbb9191a8bb693b0f19d
Daven <19781485+a39676@users.noreply.github.com>
2024-05-22 13:35:21 +0800

    Update

--- src/main/java/auxiliaryCommon/pojo/constant/ServiceMQConstant.java
+++ src/main/java/auxiliaryCommon/pojo/constant/ServiceMQConstant.java
@@ -5,7 +5,9 @@ public class ServiceMQConstant {
 	public static final String BBT_SEND_MESSAGE_QUEUE = "BBTMessageQueue";
 	public static final String CRYPTO_COIN_NORMAL_MESSAGE_QUEUE = "CryptoCoinNormalMsg";
 	public static final String CRYPTO_COIN_MID_SMALL_CAPITALIZATION_MSG = "CryptoCoinMidSmallCapMsg";
+	@Deprecated
 	public static final String CRYPTO_COIN_PRICE_RANGE_MSG = "CryptoCoinPriceRangeMsg";
+	public static final String CCM_NOTICE = "CryptoCoinCcmNotice";
 	public static final String HEART_BEAT = "HeartBeat";
 
 }
