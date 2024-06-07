2f5d2e4b839252ff74569c268a34f6e38e0b65ed
Daven <19781485+a39676@users.noreply.github.com>
2024-06-07 11:08:26 +0800

    .

--- src/main/java/finance/cryptoCoin/binance/pojo/dto/CryptoCoinBinanceUserKeysDTO.java
+++ src/main/java/finance/cryptoCoin/binance/pojo/dto/CryptoCoinBinanceUserKeysDTO.java
@@ -5,6 +5,8 @@ public class CryptoCoinBinanceUserKeysDTO {
 	private Integer localUserId;
 	private String binanceApiKey;
 	private String binanceSecretKey;
+	private String gateIoApiKey;
+	private String gateIoSecretKey;
 
 	public Integer getLocalUserId() {
 		return localUserId;
@@ -30,10 +32,27 @@ public class CryptoCoinBinanceUserKeysDTO {
 		this.binanceSecretKey = binanceSecretKey;
 	}
 
+	public String getGateIoApiKey() {
+		return gateIoApiKey;
+	}
+
+	public void setGateIoApiKey(String gateIoApiKey) {
+		this.gateIoApiKey = gateIoApiKey;
+	}
+
+	public String getGateIoSecretKey() {
+		return gateIoSecretKey;
+	}
+
+	public void setGateIoSecretKey(String gateIoSecretKey) {
+		this.gateIoSecretKey = gateIoSecretKey;
+	}
+
 	@Override
 	public String toString() {
-		return "BinanceUserKeysDTO [localUserId=" + localUserId + ", binanceApiKey=" + binanceApiKey
-				+ ", binanceSecretKey=" + binanceSecretKey + "]";
+		return "CryptoCoinBinanceUserKeysDTO [localUserId=" + localUserId + ", binanceApiKey=" + binanceApiKey
+				+ ", binanceSecretKey=" + binanceSecretKey + ", gateIoApiKey=" + gateIoApiKey + ", gateIoSecretKey="
+				+ gateIoSecretKey + "]";
 	}
 
 }
