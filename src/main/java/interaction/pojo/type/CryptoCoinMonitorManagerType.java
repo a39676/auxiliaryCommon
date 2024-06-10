652f4b2cc912ec2b674d30a153fe2988c1f85fdd
Daven <19781485+a39676@users.noreply.github.com>
2024-06-11 00:34:09 +0800

    .

--- src/main/java/interaction/pojo/type/CryptoCoinMonitorManagerType.java
+++ src/main/java/interaction/pojo/type/CryptoCoinMonitorManagerType.java
@@ -1,22 +1,22 @@
 package interaction.pojo.type;
 
 public enum CryptoCoinMonitorManagerType {
-	
+
 	REFRESH_OPTION("refreshOption", 0),
 	REFRESH_CRYPTO_COIN_OPTION("refreshCryptoCoinOption", 1),
 	REFRESH_CRYPTO_COIN_PRICE_RANGE_OPTION("refreshCryptoCoinPriceRangeOption", 2),
 	RESTART_CRYPTO_COIN_WEB_SOCKET_CLIENT("restartCryptoCoinWebSocketClient", 3),
-	CLEAN_ALL_OLD_REDIS_KEY("clearAllOldRedisKey", 4),
+	CLEAN_ALL_OLD_REDIS_KEY("clearAllOldRedisKey", 4), 
+	REFRESH_BINANCE_TRADING_OPTION("refreshBinanceTradingOption", 5);
 	;
-	
+
 	private String name;
 	private Integer code;
-	
+
 	CryptoCoinMonitorManagerType(String name, Integer code) {
 		this.name = name;
 		this.code = code;
 	}
-	
 
 	public String getName() {
 		return name;
@@ -34,10 +34,10 @@ public enum CryptoCoinMonitorManagerType {
 		}
 		return null;
 	}
-	
+
 	public static CryptoCoinMonitorManagerType getType(Integer typeCode) {
-		for(CryptoCoinMonitorManagerType t : CryptoCoinMonitorManagerType.values()) {
-			if(t.getCode().equals(typeCode)) {
+		for (CryptoCoinMonitorManagerType t : CryptoCoinMonitorManagerType.values()) {
+			if (t.getCode().equals(typeCode)) {
 				return t;
 			}
 		}
