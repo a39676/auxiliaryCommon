da120acb1eeb6142f07cf3541705154f549627bf
Daven <19781485+a39676@users.noreply.github.com>
2024-08-01 17:03:21 +0800

    .

--- src/main/java/finance/cryptoCoin/pojo/dto/CryptoCoinSymbolGroupSettingDTO.java
+++ src/main/java/finance/cryptoCoin/pojo/dto/CryptoCoinSymbolGroupSettingDTO.java
@@ -1,10 +1,11 @@
 package finance.cryptoCoin.pojo.dto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CryptoCoinSymbolGroupSettingDTO {
 
-	private List<CryptoCoinAddSymbolGroupDTO> settings;
+	private List<CryptoCoinAddSymbolGroupDTO> settings = new ArrayList<>();
 
 	public List<CryptoCoinAddSymbolGroupDTO> getSettings() {
 		return settings;
