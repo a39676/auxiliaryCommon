cde2be453944e000edec0843e812ff9fdac8cc85
Daven <19781485+a39676@users.noreply.github.com>
2024-08-15 15:56:20 +0800

    .

---
+++ src/main/java/finance/cryptoCoin/binance/pojo/constant/CcmUrlConstant.java
@@ -0,0 +1,8 @@
+package finance.cryptoCoin.binance.pojo.constant;
+
+public class CcmUrlConstant {
+
+	public static final String ROOT = "/ccm";
+	
+	public static final String SPOT_LIST = "/spotList";
+}
