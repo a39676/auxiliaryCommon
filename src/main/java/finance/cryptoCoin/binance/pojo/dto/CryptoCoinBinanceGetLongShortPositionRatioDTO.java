7db1c424beab7a77ded34ce111f912b24ae77bec
DavenChan <19781485+a39676@users.noreplay.github.com>
2024-08-19 15:16:29 +0800

    .

---
+++ src/main/java/finance/cryptoCoin/binance/pojo/dto/CryptoCoinBinanceGetLongShortPositionRatioDTO.java
@@ -0,0 +1,41 @@
+package finance.cryptoCoin.binance.pojo.dto;
+
+import finance.common.pojo.type.IntervalType;
+
+public class CryptoCoinBinanceGetLongShortPositionRatioDTO {
+
+	private String symbol;
+	private IntervalType intervalType;
+	private Integer limit;
+
+	public final String getSymbol() {
+		return symbol;
+	}
+
+	public final void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+
+	public final IntervalType getIntervalType() {
+		return intervalType;
+	}
+
+	public final void setIntervalType(IntervalType intervalType) {
+		this.intervalType = intervalType;
+	}
+
+	public final Integer getLimit() {
+		return limit;
+	}
+
+	public final void setLimit(Integer limit) {
+		this.limit = limit;
+	}
+
+	@Override
+	public String toString() {
+		return "CryptoCoinGetLongShortPositionRatioDTO [symbol=" + symbol + ", intervalType=" + intervalType
+				+ ", limit=" + limit + "]";
+	}
+
+}
---
+++ src/main/java/finance/cryptoCoin/binance/pojo/dto/CryptoCoinBinanceGetLongShortPositionRatioDetailDTO.java
@@ -0,0 +1,58 @@
+package finance.cryptoCoin.binance.pojo.dto;
+
+public class CryptoCoinBinanceGetLongShortPositionRatioDetailDTO {
+
+	private String symbol; // "BTCUSDT"
+	private Double longAccount; // "0.5352"
+	private Double longShortRatio; // "1.1514"
+	private Double shortAccount; // "0.4648"
+	private Long timestamp; // 1723766400000
+
+	public final String getSymbol() {
+		return symbol;
+	}
+
+	public final void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+
+	public final Double getLongAccount() {
+		return longAccount;
+	}
+
+	public final void setLongAccount(Double longAccount) {
+		this.longAccount = longAccount;
+	}
+
+	public final Double getLongShortRatio() {
+		return longShortRatio;
+	}
+
+	public final void setLongShortRatio(Double longShortRatio) {
+		this.longShortRatio = longShortRatio;
+	}
+
+	public final Double getShortAccount() {
+		return shortAccount;
+	}
+
+	public final void setShortAccount(Double shortAccount) {
+		this.shortAccount = shortAccount;
+	}
+
+	public final Long getTimestamp() {
+		return timestamp;
+	}
+
+	public final void setTimestamp(Long timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	@Override
+	public String toString() {
+		return "CryptoCoinGetLongShortPositionRatioDetailDTO [symbol=" + symbol + ", longAccount=" + longAccount
+				+ ", longShortRatio=" + longShortRatio + ", shortAccount=" + shortAccount + ", timestamp=" + timestamp
+				+ "]";
+	}
+
+}
