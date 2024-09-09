b6e7fb07082b8f516c37d2835cc9291275e1f593
Daven <19781485+a39676@users.noreply.github.com>
2024-09-09 16:35:48 +0800

    .

---
+++ src/main/java/tool/service/TimeBasedOneTimePassword.java
@@ -0,0 +1,38 @@
+package tool.service;
+
+import dev.samstevens.totp.code.CodeGenerator;
+import dev.samstevens.totp.code.CodeVerifier;
+import dev.samstevens.totp.code.DefaultCodeGenerator;
+import dev.samstevens.totp.code.DefaultCodeVerifier;
+import dev.samstevens.totp.exceptions.CodeGenerationException;
+import dev.samstevens.totp.time.SystemTimeProvider;
+import dev.samstevens.totp.time.TimeProvider;
+
+public class TimeBasedOneTimePassword {
+
+	private static final TimeProvider timeProvider = new SystemTimeProvider();
+	private static final CodeGenerator codeGenerator = new DefaultCodeGenerator();
+	private static final int timePeriod = 30;
+	private static final CodeVerifier verifier = new DefaultCodeVerifier(codeGenerator, timeProvider);
+
+	public static boolean isValid(String secret, String actualCode) {
+		return verifier.isValidCode(secret, actualCode);
+	}
+	
+	public static String generatorCode(String secret) {
+		long currentBucket = Math.floorDiv(timeProvider.getTime(), timePeriod);
+		try {
+			return codeGenerator.generate(secret, currentBucket);
+		} catch (CodeGenerationException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+//	public static void main(String[] args) {
+//		String secret = "WQKTNTS4QQTUSUFASVK6X44NBTRZCQBAYUIHJFNDFFDBJFKD67SHFD7U";
+//		String code = generatorCode(secret);
+//		System.out.println(code);
+//		System.out.println(isValid(secret, code));
+//	}
+}
