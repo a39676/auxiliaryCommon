c68d4c1001be2977255fd45ca8185c56ac261a04
Daven <19781485+a39676@users.noreply.github.com>
2024-09-23 20:41:52 +0800

    .

--- src/main/java/finance/cryptoCoin/binance/pojo/type/BinanceTimeInForceType.java
+++ src/main/java/finance/cryptoCoin/binance/pojo/type/BinanceTimeInForceType.java
@@ -3,19 +3,22 @@ package finance.cryptoCoin.binance.pojo.type;
 public enum BinanceTimeInForceType {
 
 	/** Good Til Canceled; An order will be on the book unless the order is canceled. */
-	GTC("GTC", 1) ,
+	GTC("GTC", 1, "一直有效直到取消") ,
 	/** Immediate Or Cancel; An order will try to fill the order as much as it can before the order expires. */
-	IOC("IOC", 2) ,
+	IOC("IOC", 2, "立即成交否则取消") ,
 	/** Fill or Kill; An order will expire if the full order cannot be filled upon execution. */
-	FOK("FOK", 3) ,
+	FOK("FOK", 3, "完全成交或取消") ,
+	GTD("GTD", 4, "有效至指定日期") ,
 	;
 
 	private String name;
 	private Integer code;
+	private String cnName;
 
-	BinanceTimeInForceType(String name, Integer code) {
+	BinanceTimeInForceType(String name, Integer code, String cnName) {
 		this.name = name;
 		this.code = code;
+		this.cnName = cnName;
 	}
 
 	public String getName() {
@@ -24,6 +27,10 @@ public enum BinanceTimeInForceType {
 
 	public Integer getCode() {
 		return code;
+	}
+	
+	public String getCnName() {
+		return cnName;
 	}
 
 	public static BinanceTimeInForceType getType(String typeName) {
