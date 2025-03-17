2898938ec034b2edea25506fe6b07011baaad8b1
Daven <19781485+a39676@users.noreply.github.com>
2025-03-17 18:52:51 +0800

    Update

--- src/main/java/finance/cryptoCoin/pojo/constant/CryptoCoinMQConstant.java
+++ src/main/java/finance/cryptoCoin/pojo/constant/CryptoCoinMQConstant.java
@@ -12,15 +12,17 @@ public class CryptoCoinMQConstant {
 	public static final String MID_SMALL_CAPITALIZATION_MSG = "CryptoCoinMidSmallCapMsg";
 	public static final String CCM_NOTICE = "CryptoCoinCcmNotice";
 	public static final String ORDER_RESULT_FEEDBACK = "CryptoCoinOrderResultFeedback";
-	
+
 	public static final String CRYPTO_COIN_SPOT_ORDER = "CryptoCoinSpotOrder";
 	public static final String CRYPTO_COIN_SPOT_CANCEL_MULTIPLE_ORDER = "CryptoCoinSpotCancelMultipleOrder";
 	public static final String CRYPTO_COIN_SPOT_CANCEL_ORDER_BY_ID = "CryptoCoinSpotCancelOrderById";
-	
+
 	public static final String BINANCE_UM_FUTURE_ORDER = "CryptoCoinBinanceUmFutureOrder";
 	public static final String BINANCE_UM_FUTURE_SINGLE_USER_GROUP_ORDER = "CryptoCoinBinanceUmFutureSingleUserGroupOrder";
 	public static final String BINANCE_UM_BTC_ARBITRAGE_WITH_BATCH = "CryptoCoinBinanceUmBtcArbitrageWithBatch";
 	public static final String BINANCE_UM_FUTURE_ORDER_MODIFY = "CryptoCoinBinanceUmFutureOrderModify";
+	public static final String BINANCE_UM_FUTURE_CANCEL_MULTIPLE_ORDER = "CryptoCoinBinanceUmFutureCancelMultipleOrder";
+	public static final String BINANCE_UM_FUTURE_CANCEL_ORDER_BY_ID = "CryptoCoinBinanceUmFutureCancelOrderById";
 
 	public static final String BINANCE_CM_FUTURE_ORDER = "CryptoCoinBinanceCmFutureOrder";
 	public static final String BINANCE_CM_FUTURE_CANCEL_MULTIPLE_ORDER = "CryptoCoinBinanceCmFutureCancelMultipleOrder";
